db45a9038bd43c7272b9bf7659a4d648641be053
BrainVader <brainvade@gmail.com>
2020-05-24 16:11:24 +0900

    Add localhost address

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,13 @@
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 #[actix_rt::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // local loop back address
+    let localhost = Ipv4Addr::new(127, 0, 0, 1);
+    let ip = IpAddr::V4(localhost);
+    assert_eq!(ip.is_loopback(), true);
+
+    let port = 8080;
+    let _addr = SocketAddr::new(ip, port);
+
     Ok(())
 }
