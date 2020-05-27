ac1bfd940dfe16cb129ea3071b21d46d579e4957
BrainVader <brainvade@gmail.com>
2020-05-27 22:48:56 +0900

    Add elasticsearch client instance

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,12 +2,17 @@ use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
 use actix_web::{web, HttpServer};
 use anyhow::Context;
-use listenfd::ListenFd;
-
 use backend_lib::setup_logger;
+use listenfd::ListenFd;
+use url::Url;
+
+use elasticsearch::http::transport::{BuildError, SingleNodeConnectionPool, TransportBuilder};
+use elasticsearch::Elasticsearch;
 
 const HOST: &'static str = "HOST";
 const PORT: &'static str = "PORT";
+const ES_HOST: &'static str = "ES_HOST";
+const ES_PORT: &'static str = "ES_PORT";
 
 mod endpoints;
 use endpoints::{hello_monq, index, page_not_found};
@@ -15,6 +20,12 @@ use endpoints::{hello_monq, index, page_not_found};
 fn get_env_var(key: &str) -> anyhow::Result<String> {
     let value = dotenv::var(key).with_context(|| format!("Failed to find key: {}", key))?;
     Ok(value)
+}
+
+fn create_elasticsearch_client(url: Url) -> Result<Elasticsearch, BuildError> {
+    let conn_pool = SingleNodeConnectionPool::new(url);
+    let transport = TransportBuilder::new(conn_pool).disable_proxy().build()?;
+    Ok(Elasticsearch::new(transport))
 }
 
 #[actix_rt::main]
@@ -53,6 +64,14 @@ async fn start_server() -> anyhow::Result<()> {
 
 fn main() -> anyhow::Result<()> {
     setup_logger().with_context(|| format!("Failed to set up logger"))?;
+
+    let es_host = get_env_var(ES_HOST)?;
+    let es_port = get_env_var(ES_PORT)?;
+    let es_addr = format!("http://{}:{}", es_host, es_port);
+    let url = Url::parse(&es_addr)?;
+    let client = create_elasticsearch_client(url)
+        .with_context(|| format!("Failed to create elasticsearch client"))?;
+
     start_server().with_context(|| format!("Failed to start server"))?;
     Ok(())
 }
