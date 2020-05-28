3829826c15c1493ccaedc50632ba23e8969b8855
BrainVader <brainvade@gmail.com>
2020-05-28 21:17:17 +0900

    Add endpoint to call cat api

--- backend/src/endpoints.rs
+++ backend/src/endpoints.rs
@@ -1,4 +1,7 @@
-use actix_web::{get, http, HttpResponse};
+use actix_web::{get, http, web, HttpResponse};
+use elasticsearch::http::response::Response;
+use elasticsearch::{Elasticsearch, Error};
+use futures::future::TryFutureExt;
 
 #[get("/")]
 pub async fn index() -> impl actix_web::Responder {
@@ -14,6 +17,33 @@ pub async fn hello_monq() -> impl actix_web::Responder {
     builder
         .content_type(mime_type.to_string())
         .body("Hello MonQ!")
+}
+
+#[get("/cat")]
+pub async fn cat(client: web::Data<Elasticsearch>) -> impl actix_web::Responder {
+    let cat_api = client.get_ref().cat();
+    let result: Result<String, Error> = cat_api
+        .nodes()
+        .h(&["ip", "port", "heapPercent", "name"])
+        .format("json")
+        .send()
+        .and_then(|response: Response| async { response.text().await })
+        .await;
+
+    match result {
+        Ok(response_body) => {
+            log::info!("response body: {:#?}", response_body);
+            let mut builder = HttpResponse::Ok();
+            let mime_type: http::header::ContentType = http::header::ContentType::json();
+            builder
+                .content_type(mime_type.to_string())
+                .json(response_body)
+        }
+        Err(e) => {
+            log::info!("failed to cat, error: {:?}", &e);
+            HttpResponse::NotFound().finish()
+        }
+    }
 }
 
 pub async fn page_not_found() -> impl actix_web::Responder {
