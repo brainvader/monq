a05a761918c83012432b06677751df8d221dff84
BrainVader <brainvade@gmail.com>
2020-06-21 22:14:45 +0900

    Add QuizController type alias

--- backend/src/context.rs
+++ backend/src/context.rs
@@ -1,4 +1,9 @@
-use elasticsearch::Elasticsearch;
+use super::clean::interface::{Controller, QuizDocumentRepository, QuizPresenter};
+use super::clean::usecases::QuizInteractor;
+use super::db::ESHandler;
+
+pub type QuizController =
+    Controller<QuizInteractor<QuizPresenter, QuizDocumentRepository<ESHandler>>>;
 
 pub struct GraphQLContext {
     pub client: Elasticsearch,
