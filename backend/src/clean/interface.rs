61591e478ffbf145d7cbdf303b7a0110cb8c3442
BrainVader <brainvade@gmail.com>
2020-06-21 12:48:16 +0900

    Add implementation for Controller

--- backend/src/clean/interface.rs
+++ backend/src/clean/interface.rs
@@ -6,3 +6,15 @@ where
 {
     pub inputport: InputPort,
 }
+
+impl<InputPort> Controller<InputPort>
+where
+    InputPort: usecases::QuizInputPort,
+{
+    pub fn download_quiz(
+        &self,
+        params: usecases::DownloadQuizRequestParams,
+    ) -> usecases::QuizDownloaded<InputPort::Output> {
+        self.inputport.download_quiz(params)
+    }
+}
