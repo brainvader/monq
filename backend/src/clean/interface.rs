3027d08c959408aebf12bff932ddff073d4c768f
BrainVader <brainvade@gmail.com>
2020-06-21 13:50:15 +0900

    Add QuizPresenter struct and its implementation

--- backend/src/clean/interface.rs
+++ backend/src/clean/interface.rs
@@ -1,3 +1,5 @@
+// FIXME: Leak entity from domain layer
+use super::entity;
 use super::usecases;
 
 pub struct Controller<InputPort>
@@ -16,5 +18,13 @@ where
         params: usecases::DownloadQuizRequestParams,
     ) -> usecases::QuizDownloaded<InputPort::Output> {
         self.inputport.download_quiz(params)
+    }
+}
+
+pub struct QuizPresenter {}
+
+impl usecases::QuizOutputPort for QuizPresenter {
+    fn downloaded_quiz(&self, quiz: entity::Quiz) -> usecases::QuizDownloaded<entity::Quiz> {
+        usecases::QuizDownloaded { source: quiz }
     }
 }
