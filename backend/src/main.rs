d19cc0ff05d2119bbf7e51f122f8a927db8864cd
BrainVader <brainvade@gmail.com>
2020-06-21 22:21:34 +0900

    Add controller instance

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,21 +1,30 @@
 use actix_web::{web, HttpServer};
 use anyhow::Context;
+use elasticsearch::Elasticsearch;
 use listenfd::ListenFd;
 
 use backend_lib::clean::{interface, usecases};
 use backend_lib::db::create_elasticsearch_client;
-
+use backend_lib::db::ESHandler;
 use backend_lib::endpoints::{cat, graphql_config, hello_monq, index, page_not_found};
-
 use backend_lib::util::{get_es_url, get_server_address, setup_logger};
 
 #[actix_rt::main]
 async fn start_server(client: Elasticsearch) -> anyhow::Result<()> {
+    let handler = ESHandler { client };
+    let repository = interface::QuizDocumentRepository { handler };
+    let output_port = interface::QuizPresenter;
+    let input_port = usecases::QuizInteractor {
+        output_port,
+        repository,
+    };
+    let controller = interface::Controller { input_port };
+
     let addr = get_server_address()?;
     // actix-web application factory
     let app_factory = move || {
         actix_web::App::new()
-            .data(client.clone())
+            .data(controller.clone())
             .wrap(actix_web::middleware::Logger::default())
             .service(index)
             .service(hello_monq)
