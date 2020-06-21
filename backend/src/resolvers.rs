29ab474c89652d0f3e76983b4fc1268b90a03262
BrainVader <brainvade@gmail.com>
2020-06-21 22:34:43 +0900

    Add QueryRoot struct

--- backend/src/resolvers.rs
+++ backend/src/resolvers.rs
@@ -1,1 +1,16 @@
+use super::clean::{entity, usecases};
+use super::context::GraphQLContext;
+use juniper::FieldResult;
 
+pub struct QueryRoot;
+
+#[juniper::object(Context = GraphQLContext)]
+impl QueryRoot {
+    fn quiz(context: &GraphQLContext, id: String) -> FieldResult<entity::Quiz> {
+        let controller = context.controller.clone();
+        let quiz_id = entity::QuizID::from(id);
+        let params = usecases::DownloadQuizRequestParams { id: quiz_id };
+        let downloaded = controller.download_quiz(params);
+        Ok(downloaded.source)
+    }
+}
