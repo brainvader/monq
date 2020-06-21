8f9bcd9395f05820f5c43830a48c60b60d2c3793
BrainVader <brainvade@gmail.com>
2020-06-21 17:36:52 +0900

    Implement ESHandle for ESHandle

--- backend/src/db.rs
+++ backend/src/db.rs
@@ -3,9 +3,13 @@
 // https://github.com/lucperkins/rust-graphql-juniper-actix-diesel-postgres/blob/master/src/db.rs
 // https://docs.rs/elasticsearch/7.7.0-alpha.1/elasticsearch/#create-a-client
 
+use async_trait::async_trait;
 use elasticsearch::http::transport::{BuildError, SingleNodeConnectionPool, TransportBuilder};
 use elasticsearch::Elasticsearch;
 use url::Url;
+
+use super::clean::entity;
+use super::clean::interface::{ESHandle, ResponseBody};
 
 pub fn create_elasticsearch_client(url: Url) -> Result<Elasticsearch, BuildError> {
     let conn_pool = SingleNodeConnectionPool::new(url);
@@ -17,3 +21,18 @@ pub struct ESHandler {
     pub client: Elasticsearch,
 }
 
+async fn get(client: &Elasticsearch, id: &entity::QuizID) -> anyhow::Result<entity::Quiz> {
+    let get_parts = elasticsearch::GetParts::IndexId("monq", id);
+    let response = client.get(get_parts).send().await?;
+    let response_body = response.json::<ResponseBody<entity::Quiz>>().await?;
+    Ok(response_body.source)
+}
+
+#[async_trait]
+impl ESHandle for ESHandler {
+    async fn get(&self, id: &entity::QuizID) -> entity::Quiz {
+        let hoge = get(&self.client, id).await;
+        // FIXME: Use anyhow or Result to handle error
+        hoge.unwrap()
+    }
+}
