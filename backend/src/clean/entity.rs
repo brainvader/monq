f450504479a6aa85c402c5c86ce805e3ed1967e2
BrainVader <brainvade@gmail.com>
2020-06-23 19:36:39 +0900

    Update entities

--- backend/src/clean/entity.rs
+++ backend/src/clean/entity.rs
@@ -1,13 +1,11 @@
-use juniper::{GraphQLInputObject, GraphQLObject};
 use serde::{Deserialize, Serialize};
 
-pub type QuizID = juniper::ID;
+pub type QuizID = String;
 pub type QuizTitle = Cell;
 pub type QuizQuestion = Vec<Cell>;
 pub type QuizAnswer = Vec<Cell>;
 
-#[derive(GraphQLObject, Deserialize, Serialize, Clone)]
-#[graphql(description = "Quiz consists of question and answer")]
+#[derive(Deserialize, Serialize, Clone)]
 pub struct Quiz {
     pub id: QuizID,
     pub title: QuizTitle,
@@ -15,28 +13,8 @@ pub struct Quiz {
     pub answer: QuizAnswer,
 }
 
-#[derive(GraphQLObject, Deserialize, Serialize, Clone)]
-#[graphql(description = "A cell contains various kinds of format data")]
+#[derive(Deserialize, Serialize, Clone)]
 pub struct Cell {
-    pub r#type: String,
-    pub content: String,
-}
-
-pub type NewID = juniper::ID;
-pub type NewTitle = NewCell;
-pub type NewQuestion = Vec<NewCell>;
-pub type NewAnswer = Vec<NewCell>;
-
-#[derive(GraphQLInputObject, Deserialize, Serialize, Clone)]
-pub struct NewQuiz {
-    pub id: NewID,
-    pub title: NewTitle,
-    pub question: NewQuestion,
-    pub answer: NewAnswer,
-}
-
-#[derive(GraphQLInputObject, Deserialize, Serialize, Clone)]
-pub struct NewCell {
     pub r#type: String,
     pub content: String,
 }
