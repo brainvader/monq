28f9cc541502295863538f6128ce776edf8705a3
BrainVader <brainvade@gmail.com>
2020-06-25 23:20:09 +0900

    Add RequestBody struct for create index api

---
+++ backend/src/api/index.rs
@@ -0,0 +1,47 @@
+use serde::Serialize;
+
+pub struct Index<'a> {
+    name: &'a str,
+    config: RequestBody<'a>,
+}
+
+#[derive(Serialize)]
+pub struct RequestBody<'a> {
+    mappings: Mappings<'a>,
+}
+
+impl<'a> Default for RequestBody<'a> {
+    fn default() -> Self {
+        let title = Property { r#type: "text" };
+        let question = Property { r#type: "text" };
+        let answer = Property { r#type: "text" };
+        let properties = Properties {
+            title,
+            question,
+            answer,
+        };
+        let mappings = Mappings { properties };
+        Self { mappings }
+    }
+}
+
+#[derive(Serialize)]
+// #[serde(rename = "mappings")]
+pub struct Mappings<'a> {
+    properties: Properties<'a>,
+}
+
+#[derive(Serialize)]
+pub struct Properties<'a> {
+    title: Property<'a>,
+    question: Property<'a>,
+    answer: Property<'a>,
+}
+
+// Mapping parameters
+// https://www.elastic.co/guide/en/elasticsearch/reference/current/mapping-params.html
+#[derive(Serialize)]
+pub struct Property<'a> {
+    r#type: &'a str,
+    // analyzer: String,
+}
