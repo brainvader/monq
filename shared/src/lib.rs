65028f2c4ae192163de4e2d564b8f7c49b0c2ffd
BrainVader <brainvade@gmail.com>
2020-07-04 20:45:26 +0900

    Add get_doc function

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -1,10 +1,19 @@
+use elasticsearch::http::response::Response;
 use elasticsearch::http::transport::{BuildError, SingleNodeConnectionPool, TransportBuilder};
 use elasticsearch::Elasticsearch;
 
 use url::Url;
 
+const INDEX_NAME: &str = "monq";
+
 pub fn create_elasticsearch_client(url: Url) -> Result<Elasticsearch, BuildError> {
     let conn_pool = SingleNodeConnectionPool::new(url);
     let transport = TransportBuilder::new(conn_pool).disable_proxy().build()?;
     Ok(Elasticsearch::new(transport))
+}
+
+pub async fn get_doc(client: &Elasticsearch, doc_id: &str) -> anyhow::Result<Response> {
+    let get_parts = elasticsearch::GetParts::IndexId(INDEX_NAME, doc_id);
+    let response = client.get(get_parts).send().await?;
+    Ok(response)
 }
