2df8504c6abe995037e437307477bf9ec6bdeba2
BrainVader <brainvade@gmail.com>
2020-07-05 12:05:34 +0900

    Support setup subcommand

--- monq_cli/src/main.rs
+++ monq_cli/src/main.rs
@@ -2,9 +2,11 @@ use clap::App;
 
 #[tokio::main]
 async fn main() {
+    let setup_cmd = App::new("setup").about("Setup index for monq");
     App::new("monq")
         .version("0.1")
         .about("command line interface for monq")
         .author("BrainVader")
+        .subcommand(setup_cmd)
         .get_matches();
 }
