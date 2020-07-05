5506a02860a84c795057757eb4c2380d61fb2a40
BrainVader <brainvade@gmail.com>
2020-07-05 12:22:15 +0900

    Add elasticsearch client

--- monq_cli/src/main.rs
+++ monq_cli/src/main.rs
@@ -1,7 +1,12 @@
+use anyhow::Context;
 use clap::App;
 
+use shared::{create_elasticsearch_client, get_es_url};
 #[tokio::main]
 async fn main() {
+    let url = get_es_url().with_context(|| "failed to get elasticsearch url")?;
+    let client = create_elasticsearch_client(url)
+        .with_context(|| "failed to create elasticsearch client")?;
     let setup_cmd = App::new("setup").about("Setup index for monq");
     App::new("monq")
         .version("0.1")
