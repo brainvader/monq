bb5ea8a4862154e448edc3cffd4a056d0c4723d8
BrainVader <brainvade@gmail.com>
2020-07-05 12:22:49 +0900

    Add logger

--- monq_cli/src/main.rs
+++ monq_cli/src/main.rs
@@ -2,8 +2,11 @@ use anyhow::Context;
 use clap::App;
 
 use shared::{create_elasticsearch_client, get_es_url};
+use shared::{log_info, setup_logger};
+
 #[tokio::main]
 async fn main() {
+    setup_logger().with_context(|| "failed to setup logger")?;
     let url = get_es_url().with_context(|| "failed to get elasticsearch url")?;
     let client = create_elasticsearch_client(url)
         .with_context(|| "failed to create elasticsearch client")?;
