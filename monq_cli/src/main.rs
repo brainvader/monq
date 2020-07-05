57f43b33f85d4dd25b78ea42030b4463c2cae6fd
BrainVader <brainvade@gmail.com>
2020-07-05 11:58:20 +0900

    Update command name to monq

--- monq_cli/src/main.rs
+++ monq_cli/src/main.rs
@@ -2,7 +2,7 @@ use clap::App;
 
 #[tokio::main]
 async fn main() {
-    App::new("cli")
+    App::new("monq")
         .version("0.1")
         .about("command line interface for monq")
         .author("BrainVader")
