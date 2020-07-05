7e06baaac961e6c5f06b188e05eafb447100067c
BrainVader <brainvade@gmail.com>
2020-07-05 09:02:05 +0900

    Move setup_logger function from backend to shared

--- backend/src/util.rs
+++ backend/src/util.rs
@@ -1,16 +1,6 @@
-use env_logger::{DEFAULT_FILTER_ENV, DEFAULT_WRITE_STYLE_ENV};
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
 use shared::get_env_var;
-
-pub fn setup_logger() -> dotenv::Result<()> {
-    let log_level: String = dotenv::var(DEFAULT_FILTER_ENV)?;
-    let log_style: String = dotenv::var(DEFAULT_WRITE_STYLE_ENV)?;
-    std::env::set_var(DEFAULT_FILTER_ENV, log_level);
-    std::env::set_var(DEFAULT_WRITE_STYLE_ENV, log_style);
-    env_logger::builder().init();
-    Ok(())
-}
 
 const HOST: &str = "HOST";
 const PORT: &str = "PORT";
--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -6,9 +6,20 @@ use anyhow::Context;
 use serde::Serialize;
 use url::Url;
 
+use env_logger::{DEFAULT_FILTER_ENV, DEFAULT_WRITE_STYLE_ENV};
+
 const INDEX_NAME: &str = "monq";
 const ES_HOST: &str = "ES_HOST";
 const ES_PORT: &str = "ES_PORT";
+
+pub fn setup_logger() -> dotenv::Result<()> {
+    let log_level: String = dotenv::var(DEFAULT_FILTER_ENV)?;
+    let log_style: String = dotenv::var(DEFAULT_WRITE_STYLE_ENV)?;
+    std::env::set_var(DEFAULT_FILTER_ENV, log_level);
+    std::env::set_var(DEFAULT_WRITE_STYLE_ENV, log_style);
+    env_logger::builder().init();
+    Ok(())
+}
 
 pub fn get_env_var(key: &str) -> anyhow::Result<String> {
     let value = dotenv::var(key).with_context(|| format!("Failed to find key: {}", key))?;
