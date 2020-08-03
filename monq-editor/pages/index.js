bc67f65f92e0cf73c60045226066b2ee9d65eb90
brainvader <brainvade@gmail.com>
2020-08-03 18:37:02 +0900

    Use Layout component

--- monq-editor/pages/index.js
+++ monq-editor/pages/index.js
@@ -1,9 +1,10 @@
+import Layout from '../components/layout'
 import Nav from '../components/nav'
 import { FaBeer } from 'react-icons/fa';
 
 export default function Editor() {
   return (
-    <div>
+    <Layout>
       <Nav />
       <div className="py-20">
         <h1 className="text-5xl text-center text-accent-1">
@@ -11,6 +12,6 @@ export default function Editor() {
         </h1>
         <div className="flex justify-center leading-none text-2xl">これがアイコンです<span><FaBeer className="bg-orange-500" /></span></div>
       </div>
-    </div >
+    </Layout>
   )
 }
