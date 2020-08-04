a03ac74b1f0070499b3073480ff3685c9604953e
brainvader <brainvade@gmail.com>
2020-08-04 21:18:23 +0900

    Use HighlightJS component

--- monq-editor/pages/index.js
+++ monq-editor/pages/index.js
@@ -4,6 +4,25 @@ import Layout from '../components/layout'
 import Nav from '../components/nav'
 import { FaBeer } from 'react-icons/fa';
 
+import HighlightJS from '../components/highlightjs'
+
+const codeSample = `#[derive(Debug)]
+pub enum State {
+    Start,
+    Transient,
+    Closed,
+}
+
+impl From<&'a str> for State {
+    fn from(s: &'a str) -> Self {
+        match s {
+            "start" => State::Start,
+            "closed" => State::Closed,
+            _ => unreachable!(),
+        }
+    }
+}`
+
 export default function Editor() {
   return (
     <Layout>
@@ -15,7 +34,12 @@ export default function Editor() {
         <h1 className="text-5xl text-center text-accent-1">
           Next.js + Tailwind CSS
         </h1>
-        <div className="flex justify-center leading-none text-2xl">これがアイコンです<span><FaBeer className="bg-orange-500" /></span></div>
+        <div className="flex justify-center leading-none text-2xl">
+          これがアイコンです<span><FaBeer className="bg-orange-500" /></span>
+        </div>
+        <div>
+          <HighlightJS codeString={codeSample} language="rust" />
+        </div>
       </div>
     </Layout>
   )
