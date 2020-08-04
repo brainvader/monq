2e31dcba362391b8706a0f4dde4e198df3615653
brainvader <brainvade@gmail.com>
2020-08-04 22:15:48 +0900

    Import style for katex math equation

--- monq-editor/pages/_app.js
+++ monq-editor/pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/index.css'
 import 'highlight.js/styles/monokai-sublime.css';
+import 'katex/dist/katex.css'
 
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />
